d4d77245135910507170b513026138d7dd116df6
kiu_kvirkvelia.nana <kvirkvelia.nana@kiu.edu.ge>
2025-06-01 23:08:24 +0400

    set up Express server with /ping route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
+import express from "express";
 
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
