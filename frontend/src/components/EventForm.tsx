eae5b0933be53f786823633cbcd58c1428dc8384
DESKTOP-HCMJU23\kiustudent <ubilava.anna@kiu.edu.ge>
2025-06-01 21:34:11 +0400

    placeholder eventform

--- frontend/src/components/EventForm.tsx
+++ frontend/src/components/EventForm.tsx
@@ -1,7 +1,15 @@
 import { Event } from "../types/Event";
 
 const EventForm = () => {
-  return <form></form>;
+  return (
+    <form>
+      <input type="text" placeholder="Title" required />
+      <input type="date" required />
+      <input type="text" placeholder="Location" required />
+      <textarea placeholder="Description" />
+      <button type="submit">Add Event</button>
+    </form>
+  );
 };
 
 export default EventForm;
