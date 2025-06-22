1333e8359e9684a76ac04bd909c6be38543c3f5c
kiu_kvirkvelia.nana <kvirkvelia.nana@kiu.edu.ge>
2025-06-22 12:58:19 +0400

    create CalendarPage component with basic scaffold

---
+++ frontend/src/components/CalendarPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CalendarPage: React.FC = () => {
+  return (
+    <div className="page-container">
+      <h1>Event Calendar</h1>
+      <p>Select a date to view events.</p>
+      {/* Calendar UI will go here */}
+    </div>
+  );
+};
+
+export default CalendarPage;
